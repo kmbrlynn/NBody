659a4fa62489581e54ba0aec9b00e4c7f7efe1ef
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-02-17 18:07:07 -0500

    add time args

--- NBody.cpp
+++ NBody.cpp
@@ -9,18 +9,24 @@
 int main(int argc, char* argv[]) // read time and delta_time
 {
 	std::string data;
-	int n_bodies;
-	double universe_radius;
+	
+	int n_bodies;				// N 
+	double universe_radius;		// R
+	double time;				// T - argv[1]
+	double delta_time;			// t - argv[2]
 	
 	std::getline(std::cin, data);
 	std::istringstream(data) >> n_bodies;	
-	
 	std::getline(std::cin, data);
 	std::istringstream(data) >> universe_radius;
-	
+	std::istringstream(argv[1]) >> time;
+	std::istringstream(argv[2]) >> delta_time;
+
 	std::cout << "Number of bodies: " << n_bodies << std::endl;
 	std::cout << "Universe radius: " << universe_radius << std::endl;
 
+
+	// ======================================================================== SFML
 
 	sf::RenderWindow window(sf::VideoMode(800, 800), "N-Body Simulation");
 	window.setPosition(sf::Vector2i(200, 50));
