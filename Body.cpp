baf9d7a3290e7e9e9eda1a54bef78878647cd84f
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-02-18 16:05:23 -0500

    remove texture/sprite from constructor

---
+++ Body.cpp
@@ -0,0 +1,53 @@
+#include <SFML/Graphics.hpp>
+#include "Body.hpp"
+#include <iostream>
+
+Body::Body(double xpos, double ypos, double xvel, double yvel, 
+	       double mass, std::string filename) :
+						_xpos(xpos), _ypos(ypos), _xvel(xvel), _yvel(yvel), 
+						_mass(mass), _filename(filename)
+{
+
+
+
+}
+
+Body::Body()
+{}
+
+Body::~Body()
+{}
+
+
+// ====================================================================== overloaded
+void Body::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+
+
+}
+
+std::istream& operator >>(std::istream& in_stream, Body& body)
+{
+	in_stream >> body._xpos;
+	in_stream >> body._ypos;
+	in_stream >> body._xvel;
+	in_stream >> body._yvel;
+	in_stream >> body._mass;
+	in_stream >> body._filename;
+
+	return in_stream;
+}
+
+std::ostream& operator <<(std::ostream& out_stream, const Body& body)
+{
+	out_stream << " " << body._xpos << " ";
+	out_stream << " " << body._ypos << " ";
+	out_stream << " " << body._xvel << " ";
+	out_stream << " " << body._yvel << " ";
+	out_stream << " " << body._mass << " ";
+	out_stream << " " << body._filename << " ";
+
+	return out_stream;
+}
+
+
