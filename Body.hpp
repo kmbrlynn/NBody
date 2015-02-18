0c931f669706e8bef47ac908abfca241040471aa
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-02-18 16:05:50 -0500

    add istream overloading

--- Body.hpp
+++ Body.hpp
@@ -3,62 +3,35 @@
 class Usage
 {};
 
-class Body : public sf::Drawable
+class Body: public sf::Drawable
 {
 public:
+
+	Body(double, double, double, double, double, std::string);
+	Body();
+	~Body();
 
 	friend std::ostream& operator <<(std::ostream&, const Body&);
 	friend std::istream& operator >>(std::istream&, Body&);
 
 private:
 
-	void draw(sf::RenderTarget&, sf::RenderStates) const;
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
 	double _xpos;
 	double _ypos;
 	
-	double _xveloc;
-	double _yveloc;
+	double _xvel;
+	double _yvel;
 	
 	double _mass;
 	std::string _filename;
 
+	sf::Sprite _sprite;
+	sf::Texture _texture;
 
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
